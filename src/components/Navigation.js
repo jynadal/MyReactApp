55ad885d4b70b8df9f86cf35acf5e033b18b17ed
jynadal <nadaljeanyves@hotmail.fr>
2020-10-14 19:40:36 +0200

    setting SCSS for responsive app

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,23 +25,21 @@ export default function Navigation({user}) {
     return (
         <nav className="site-navigation">
             <span className="menu-title">My React Blog</span>
-            <div className="menu-content-container">
+            <div className={`menu-content-container ${menuActive && 'active'} `}>
                 <ul>
                     {navLinks.map((link, index) => (
                         <li key={index} >
                         <Link to={link.path}>{link.title}</Link>
                         </li>
-
                     ))                    
                     }
                 </ul>
-                <span>
+                <span className="menu-avatar-container">
                 <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38} />
                 <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
                 </span>
-                <i className="ionicons icon ion-ios-menu"  onClick={()=> setMenuActive(!menuActive)} />
             </div>
-
+            <i className="ionicons icon ion-ios-menu"  onClick={()=> setMenuActive(!menuActive)} />
         </nav>
     )
 }
